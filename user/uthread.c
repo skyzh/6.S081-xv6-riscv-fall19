a1acea23160a0cf2b5ee1c6cc4bf5dd5af998442
Alex Chi <iskyzh@gmail.com>
2019-11-19 20:13:24 +0800

    add uthread

--- user/uthread.c
+++ user/uthread.c
@@ -11,6 +11,24 @@
 #define MAX_THREAD  4
 
 struct thread {
+  uint64 ra;
+  uint64 sp;
+
+  // callee-saved
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+
+  // thread information
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
 };
@@ -53,14 +71,11 @@ thread_schedule(void)
     exit(-1);
   }
 
-  if (current_thread != next_thread) {         /* switch threads?  */
+  if (current_thread != next_thread) {
     next_thread->state = RUNNING;
     t = current_thread;
     current_thread = next_thread;
-    /* YOUR CODE HERE
-     * Invoke thread_switch to switch from t to next_thread:
-     * thread_switch(??, ??);
-     */
+    thread_switch((uint64) t, (uint64) current_thread);
   } else
     next_thread = 0;
 }
@@ -74,7 +89,8 @@ thread_create(void (*func)())
     if (t->state == FREE) break;
   }
   t->state = RUNNABLE;
-  // YOUR CODE HERE
+  t->ra = (uint64) func;
+  t->sp = (uint64) &t->stack + STACK_SIZE;
 }
 
 void 
